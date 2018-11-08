8786b738a458a5efe2df11d3047ed234b150d043
张臻 <zhangzhen11@jd.com>
2018-11-08 20:06:10 +0800

    Update

--- src/core/util/lang.js
+++ src/core/util/lang.js
@@ -25,10 +25,12 @@ export function def (obj: Object, key: string, val: any, enumerable?: boolean) {
  */
 const bailRE = /[^\w.$]/
 export function parsePath (path: string): any {
+  // 路径可能为：'a.b.c'、'$a.b.c'，不属于这种的都是错误路径
   if (bailRE.test(path)) {
     return
   }
   const segments = path.split('.')
+  // 返回obj.a.b.c的值
   return function (obj) {
     for (let i = 0; i < segments.length; i++) {
       if (!obj) return
