c17b9352ca45fa995780f71ff8e83359e63e4442
张臻 <zhangzhen11@jd.com>
2019-02-21 17:51:15 +0800

    feat: update

--- src/core/instance/proxy.js
+++ src/core/instance/proxy.js
@@ -34,11 +34,13 @@ if (process.env.NODE_ENV !== 'production') {
     )
   }
 
+  // 判断当前环境是否支持原生Proxy
   const hasProxy =
     typeof Proxy !== 'undefined' && isNative(Proxy)
 
   if (hasProxy) {
     const isBuiltInModifier = makeMap('stop,prevent,self,ctrl,shift,alt,meta,exact')
+    // 防止开发者在自定义修饰符时，覆盖了内置的修饰符
     config.keyCodes = new Proxy(config.keyCodes, {
       set (target, key, value) {
         if (isBuiltInModifier(key)) {
@@ -52,10 +54,15 @@ if (process.env.NODE_ENV !== 'production') {
     })
   }
 
+  // TODO 看完render再回来看。。。
   const hasHandler = {
     has (target, key) {
+      // 该key存在于目标对象的实例和原型中
       const has = key in target
+      // 该key是全局允许访问的关键字（在模板中允许使用全局方法，会用到Number()之类） || 该key是目标对象上自带"_"的私有属性，即不是用户声明的
       const isAllowed = allowedGlobals(key) ||
+        // key有可能为Symbol，由于Symbol没有charAt方法，因此要判断key是否是string（https://github.com/vuejs/vue/pull/7878）
+        // TODO 验证：在渲染函数会包含_c、_v等内部方法，这里需要允许
         (typeof key === 'string' && key.charAt(0) === '_' && !(key in target.$data))
       if (!has && !isAllowed) {
         if (key in target.$data) warnReservedPrefix(target, key)
@@ -79,6 +86,8 @@ if (process.env.NODE_ENV !== 'production') {
     if (hasProxy) {
       // determine which proxy handler to use
       const options = vm.$options
+      // 只有测试代码中，_withStripped才是true，因此处理函数为hasHandler
+      // TODO 手动书写的render函数并不会被包裹在with语句块内，触发不了has拦截。由于render._withStripped也未定义，也不会被get拦截，这个时候访问了不存在的this.a，无法得到警告
       const handlers = options.render && options.render._withStripped
         ? getHandler
         : hasHandler
