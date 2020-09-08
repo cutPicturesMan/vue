5fb43315fec350c9444b8dc9b43d1a465c66cafd
臻 <408869403@qq.com>
2020-09-08 18:23:14 +0800

    feat: u

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -8,6 +8,21 @@ import { createCompilerCreator } from './create-compiler'
 // `createCompilerCreator` allows creating compilers that use alternative
 // parser/optimizer/codegen, e.g the SSR optimizing compiler.
 // Here we just export a default compiler using the default parts.
+
+/*
+ 整体流程：
+  -> template字符串
+  -> parse（获得ast）
+  -> optimize（优化ast）
+  -> generate（生成render字符串）
+  -> compiler（生成render函数）
+  -> render（生成vnode）
+  -> patch（比较vnode）
+  -> update（更新页面）
+  -> 渲染到页面？
+
+  TODO AST和VNode有啥差别
+ */
 export const createCompiler = createCompilerCreator(function baseCompile (
   template: string,
   options: CompilerOptions
